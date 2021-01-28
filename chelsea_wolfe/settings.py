351da95fcf658cb2d8a1c1a54e8349a644245cfe
AudreyLL88 <a.lloancy@gmail.com>
2021-01-28 15:53:02 +0000

    Remove secret key et debug

--- chelsea_wolfe/settings.py
+++ chelsea_wolfe/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uy+q(w02pro9lk04@8wd!1z8mhry0zp)#fdxgc+@6#ylcb1d(z'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['chelsea-wolfe-store.herokuapp.com', 'localhost']
 
